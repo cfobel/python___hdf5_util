f53b0fe93def93a63cae778e2adcd8f492305200
Ryan Pattison <ryan.m.pattison@gmail.com>
2012-07-23 14:52:32 -0400

    added tuple tree notation for easily creating tree structured files and a nameless table function T

--- hdf5_util/append_files.py
+++ hdf5_util/append_files.py
@@ -5,19 +5,99 @@ from path import path
 import sys
 from subprocess import call, check_output
 import re
+from .util import find_command
+import numpy as np
+from path import path
 
-from . import find_command
+
+"""
+    because of the metaIsDescription metaclass
+    the attributes of the Table class have to be specified during it's definition.
+    we could only manage this with a string dynamically created.
+"""
+def T(**kwargs):
+    attrs = '\n'.join(['    %s = %s' % (name, repr(_type)) for name, _type in kwargs.items()])
+    class_str = '''\
+class Table(IsDescription):
+%s
+    ''' % (attrs)
+    exec(class_str)
+    return eval('Table')
+
+def python_to_numpy(variable):
+    type_map = {int:np.int64, str:np.str, float:np.float, list:np.array}
+    if type(variable) in type_map:
+        return type_map[type(variable)](variable)
+    else:
+        return variable
+
+def name(name, make_natural=True):
+    if not make_natural:
+        return name
+    nat_name = re.sub(r'[^0-9a-zA-z]', '_', name)
+    if re.match(r'[0-9]', nat_name[0]):
+        return '_' + nat_name
+    else:
+        return nat_name
+
+def hdf5_type(instance):
+    hdf5_types = [VLArray, Array, CArray, EArray, IsDescription]
+    try:
+        types =  [t for t in hdf5_types if issubclass(instance, t)]
+    except TypeError:
+        return None
+    if len(types):
+        return types[0]
+    else:
+        None
+
+def get_create(hdf5, hdf5_type):
+    type_map = {VLArray:'VLArray', Array:'Array', EArray:'EArray', 'CArray':CArray, IsDescription:'Table'}
+    return getattr(hdf5, 'create' + type_map[hdf5_type])
+
+def _create_subtree(hdf5, parent, node, force_natural=True):
+    if isinstance(node[1], tuple):
+        new_parent = hdf5.createGroup(parent, name(node[0], force_natural))
+        for child in node[1:]:
+            _create_subtree(hdf5, new_parent, child)
+    else:
+        type_ = hdf5_type(node[1])
+        if not type_:
+            parent._v_attrs[node[0]] = python_to_numpy(node[1])
+        else:
+            if type_ == IsDescription:
+                get_create(hdf5, type_)(parent, name(node[0], force_natural), node[1])
+            else:
+                get_create(hdf5, type_)(parent, name(node[0], force_natural), node[2])
 
 
 class HDF5File(object):
     script = find_command('ptrepack')
 
-    def __init__(self, filepath):
+    @staticmethod
+    def create(filepath, tree):
         # create the master file
+        HDF5File._createTree(filepath, tree).close()
+        return HDF5File(filepath)
+
+    def __init__(self, filepath):
         self.filepath = filepath
-        self.master = openFile(str(filepath), 'a')
+        self.master = openFile(filepath, 'a')
         self.master_leaves = self.get_leaves()
         self.master_nodes = self.get_nodes()
+
+    def get_leaf(self, name):
+        for leaf in self.master_leaves:
+            if path(leaf).namebase == name:
+                return self.master.getNode(leaf)
+        return None
+
+    @staticmethod
+    def _createTree(file_path, structure):
+        file_path = path(file_path)
+        hdf5 = openFile(file_path, 'w')
+        _create_subtree(hdf5, hdf5.root, structure)
+        return hdf5
 
     def get_leaves(self):
         return set([x._v_pathname for x in self.master.walkNodes()
@@ -42,9 +122,9 @@ class HDF5File(object):
             natural = re.sub(r'[^0-9A-Za-z_]', '_', name)
             if re.match(r'[0-9]', natural[0]):
                 natural = '_' + natural
-            if natural != name:        
+            if natural != name:
                 self.master.renameNode(node, natural)
-        
+
     def _append_data(self, h5f_input):
         # TODO Add check for id_ and id_ end to keep them up to date.
         # For nodes that already exist in the master, append data
@@ -58,22 +138,21 @@ class HDF5File(object):
                         row = mtable[-1]
                         if 'id_' in atable.colnames:
                             prev_id = mtable[-2]['id_']
+                            if i == 0:
+                                base = prev_id
                             if 'id_len' in atable.colnames:
                                 row['id_'] = prev_id + mtable[-2]['id_len']
                             else:
                                 # offset the first index.
-                                if i == 0:
-                                    row['id_'] = prev_id + row['id_'] + 1
-                                else:
-                                    row['id_'] = prev_id + row['id_']
-                                    
-                            mtable[-1]=[row] # just part of the mystery 
+                                row['id_'] = base + row['id_'] + 1
+
+                            mtable[-1]=[row] # just part of the mystery
                                              # of the hdf5 library
                 else:
                     # must be an Array.
                     for row in atable:
                         mtable.append(row)
-    
+
     def _copy_new_data(self, h5f_input, input_filepath):
         # For nodes that do not already exist in the master, copy the
         # nodes to the master
@@ -90,6 +169,5 @@ class HDF5File(object):
                 # Update list of master nodes, since we added a new node
                 self.master_nodes.update([n._v_pathname
                         for n in self.master.walkNodes(name)])
-
     def __del__(self):
         self.master.close()
---
+++ hdf5_util/hdf5_create.py
@@ -0,0 +1,108 @@
+#/usr/bin/env python
+import re
+from tables  import *
+import numpy as np
+from path import path
+
+
+"""
+    because of the metaIsDescription metaclass
+    the attributes of the Table class have to be specified during it's definition.
+    we could only manage this with a string dynamically created.
+"""
+def T(**kwargs):
+    attrs = '\n'.join(['    %s = %s' % (name, repr(_type)) for name, _type in kwargs.items()])
+    class_str = '''\
+class Table(IsDescription):
+%s
+    ''' % (attrs)
+    exec(class_str)
+    return eval('Table')
+
+
+
+def hdf5_createTree(file_path, structure):
+    file_path = path(file_path)
+    hdf5 = openFile(file_path, 'w')
+    _create_subtree(hdf5, hdf5.root, structure)
+    return hdf5
+
+
+def hdf5_type(instance):
+    hdf5_types = [VLArray, Array, CArray, EArray, IsDescription]
+    try:
+        types =  [t for t in hdf5_types if issubclass(instance, t)]
+    except TypeError:
+        return None
+    if len(types):
+        return types[0]
+    else:
+        None
+
+
+def get_create(hdf5, hdf5_type):
+    type_map = {VLArray:'VLArray', Array:'Array', EArray:'EArray', 'CArray':CArray, IsDescription:'Table'}
+    return getattr(hdf5, 'create' + type_map[hdf5_type])
+
+
+def _create_subtree(hdf5, parent, node):
+    if isinstance(node[1], tuple):
+        new_parent = hdf5.createGroup(parent, node[0])
+        for child in node[1:]:
+            _create_subtree(hdf5, new_parent, child)
+    else:
+        type_ = hdf5_type(node[1])
+        if not type_:
+            parent._v_attrs[node[0]] = node[1]
+        else:
+            if type_ == IsDescription:
+                get_create(hdf5, type_)(parent, node[0], node[1])
+            else:
+                get_create(hdf5, type_)(parent, node[0], node[2])
+
+def get_node(hdf5, **args):
+    return _traverse(hdf5.root, args)
+
+def _traverse(parent, nexts):
+    if not nexts:
+        return parent
+    else:
+        return _traverse(getattr(parent, nexts[0]), nexts[1:])
+
+def main(net=path('tseng.net'), algo='BestOverallXover', pop_size=256, tournament_size=32):
+    filename = net.namebase + '_' + '.h5'
+    net_nat = re.sub(r'[\-\.]', '_', net.namebase)
+
+    h = hdf5_createTree(filename,
+            (net_nat, ('_value', np.str(net.namebase)),
+                (algo,
+                    ('pop_size_%d' % pop_size, ('_value', pop_size),
+                        ('tournament_size_%d' % tournament_size, ('_value', np.int32(tournament_size)),
+                            ('params', T(netlist=StringCol(32),
+                                    arch=StringCol(32),
+                                    pop_size=UInt32Col(),
+                                    tournament_size=UInt32Col(),
+                                    num_non_improving=UInt32Col(),
+                                    stop_threshold=Float32Col())),
+                            ('results', T(generation = Int16Col(pos=0),
+                                        min_cost = Float32Col(pos=1),
+                                        mean_cost = Float32Col(pos=2),
+                                        max_cost = Float32Col(pos=3),
+                                        num_moves = UInt16Col(pos=4),
+                                        num_xovers = UInt16Col(pos=5),
+                                        run_time = Float32Col(pos=6),
+                                        memory = UInt32Col(pos=7))),
+                            ('costs', VLArray, Float32Atom(dflt=-1)),
+                            ('grids', VLArray, Int32Atom(dflt=-1))
+                        )))))
+
+    tournament = (h, net_nat, algo, 'pop_size_%d' % pop_size, 'tournament_size_%d' % tournament_size)
+    results = get_node(tournament, 'results')
+    grids = get_node(tournament, 'grids')
+    costs = get_node(tournament, 'costs')
+
+    return h
+
+if __name__ == "__main__":
+    h = main()
+    h.close()
